5c65176cc4b2d9367d476eb8cea7bb7c61bc5652
unknown <kimjiyoon0728@naver.com>
2023-04-04 15:50:24 +0900

    sliver app 완료

---
+++ lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:silver_app/ad.dart';
+import 'package:silver_app/diary.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            snap: true,
+            floating: true,
+            title: Text("SliverAppbar"),
+            pinned: false,
+            expandedHeight: 250,
+            flexibleSpace: Container(
+              child: Center(
+                child: Text(
+                  "FlexibleSpace",
+                  style: TextStyle(fontSize: 50),
+                ),
+              ),
+            ),
+          ),
+          SliverAppBar(
+            title: Text("SliverSubAppbar"),
+            pinned: true,
+          ),
+          SliverPersistentHeader(
+            delegate: MyPersistentHeaderDelegate(
+              minHeight: 60.0,
+              maxHeight: 200.0,
+            ),
+            pinned: true,
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 200,
+              color: Colors.red,
+            ),
+          ),
+          buildSliverFillViewport(),
+          SliverGrid.builder(
+            itemCount: 6,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, mainAxisSpacing: 10, crossAxisSpacing: 10),
+            itemBuilder: (context, index) {
+              return Container(
+                color: Colors.grey,
+                child: Text("$index"),
+              );
+            },
+          ),
+          SliverFixedExtentList(
+            itemExtent: 100,
+            delegate: SliverChildBuilderDelegate(
+              childCount: 50,
+                  (context, index) {
+                if (index % 4 == 0 && index != 0) {
+                  return Ad((index / 4).toInt());
+                } else {
+                  return Diary(index);
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SliverFillViewport buildSliverFillViewport() {
+    return SliverFillViewport(
+      delegate: SliverChildBuilderDelegate(
+        childCount: 2,
+            (context, index) {
+          return Container(
+            height: 50,
+            alignment: Alignment.center,
+            color: Colors.lightBlue[100 * (index % 9)],
+            child: Text("List Item $index"),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
+class MyPersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final double minHeight;
+  final double maxHeight;
+
+  MyPersistentHeaderDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+  });
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.blueGrey,
+      child: Center(
+        child: Text(
+          'SliverPersistentHeader',
+          style: TextStyle(fontSize: 20.0, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => maxHeight;
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
